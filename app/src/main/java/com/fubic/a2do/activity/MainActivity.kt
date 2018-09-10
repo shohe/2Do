7806cc194deda7dd33aca6efffac39d3fd9a381a
大谷昇平 <>
2018-09-10 19:18:18 +0900

    add addButton

--- app/src/main/java/com/fubic/a2do/activity/MainActivity.kt
+++ app/src/main/java/com/fubic/a2do/activity/MainActivity.kt
@@ -2,26 +2,27 @@ package com.fubic.a2do.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.util.Log
 import android.widget.ListView
 import com.fubic.a2do.R
 import com.fubic.a2do.adapter.TodoListAdapter
 import com.fubic.a2do.item.TodoListItem
-import com.fubic.a2do.view.TodoListView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    var todoListView: ListView? = null
+    // XMLOutlet
+    private var todoListView: ListView? = null
+    private var itemAdapter: TodoListAdapter? = null
+    private var addButton: FloatingActionButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         this.initListView()
-//        val todoListView = TodoListView(applicationContext)
-//        val item = TodoListItem(0, "studying kotlin", "office", Date())
-//        todoListView.setTodo(item)
-//        setContentView(todoListView)
+        this.initAddButton()
     }
 
     fun initListView() {
@@ -32,7 +33,16 @@ class MainActivity : AppCompatActivity() {
             val item = TodoListItem(i, "studying kotlin", "office", Date())
             items.add(item)
         }
-        var adapter = TodoListAdapter(applicationContext, items)
-        this.todoListView!!.adapter = adapter
+        this.itemAdapter = TodoListAdapter(applicationContext, items)
+        this.todoListView!!.adapter = this.itemAdapter
     }
+
+    fun initAddButton() {
+        this.addButton = findViewById(R.id.addButton) as FloatingActionButton
+        this.addButton!!.setOnClickListener {
+            Log.d("MainActivity", "msg")
+        }
+    }
+
+
 }
