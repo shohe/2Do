6f1fc8931eaeb3bd0195ada5e3010d85d512dd98
大谷昇平 <>
2018-09-10 15:11:21 +0900

    first commit

---
+++ app/src/main/java/com/fubic/a2do/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.fubic.a2do.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.fubic.a2do.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/fubic/a2do/item/TodoListItem.kt
@@ -0,0 +1,11 @@
+package com.fubic.a2do.item
+
+/**
+ * Created by shoheohtani on 2018/09/10.
+ */
+
+class TodoListItem {
+
+
+
+}
