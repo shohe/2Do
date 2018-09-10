5b00dd6e09bfa27038f50ddd54b9fc80279a5696
大谷昇平 <>
2018-09-10 16:11:04 +0900

    create Adapter class

---
+++ app/src/main/java/com/fubic/a2do/adapter/TodoListAdapter.kt
@@ -0,0 +1,32 @@
+package com.fubic.a2do.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+/**
+ * Created by shoheohtani on 2018/09/10.
+ */
+
+class TodoListAdapter : BaseAdapter() {
+
+    init {
+
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItem(p0: Int): Any {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemId(p0: Int): Long {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
