148385f3ad41819f1801870a61874d0c71e7f0af
大谷昇平 <>
2018-09-12 12:23:26 +0900

    add the function delete one of lists which was tapped

--- app/src/main/java/com/fubic/a2do/activity/MainActivity.kt
+++ app/src/main/java/com/fubic/a2do/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fubic.a2do.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
+import android.util.Log
 import android.view.View
 import android.widget.*
 import com.fubic.a2do.R
@@ -12,7 +13,10 @@ import com.fubic.a2do.view.DatePick
 import com.fubic.a2do.view.NewTodoDialog
 import com.fubic.a2do.view.NewTodoDialogDelegate
 import java.util.*
-
+import android.content.Intent
+import android.widget.Toast
+import android.widget.AdapterView
+import com.fubic.a2do.view.TodoListView
 
 
 class MainActivity : AppCompatActivity(), NewTodoDialogDelegate {
@@ -32,21 +36,23 @@ class MainActivity : AppCompatActivity(), NewTodoDialogDelegate {
 
         this.initAddButton()
         this.initListView()
+        this.addTapHandler(this.todoListView)
+        this.addLongTapHandler(this.todoListView)
     }
 
 
-    fun initListView() {
+    private fun initListView() {
         val items = arrayListOf<TodoListItem>()
         for (i in 0..5) {
             val item = TodoListItem(i, "studying kotlin", "office", Date())
             items.add(item)
         }
-        this.itemAdapter = TodoListAdapter(applicationContext, items)
+        this.itemAdapter = TodoListAdapter(this, items)
         this.todoListView.adapter = this.itemAdapter
     }
 
 
-    fun initAddButton() {
+    private fun initAddButton() {
         this.addButton.setOnClickListener {
             val ntd = NewTodoDialog(this)
             ntd.delegate = this
@@ -55,10 +61,27 @@ class MainActivity : AppCompatActivity(), NewTodoDialogDelegate {
     }
 
 
-    fun showDatePickerDialog(v: View) {
-        val fragment = DatePick()
-        fragment.show(fragmentManager, "datePicker")
+    private fun addTapHandler(listView: ListView) {
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val item = listView.adapter.getItem(position) as TodoListItem
+            Log.d("item id:", "${item.id}")
+        }
     }
+
+
+    private fun addLongTapHandler(listView: ListView) {
+        listView.setOnItemLongClickListener { parent, view, position, id ->
+            this.itemAdapter!!.items.remove(this.itemAdapter!!.getItem(position))
+            this.itemAdapter!!.notifyDataSetChanged()
+            return@setOnItemLongClickListener true
+        }
+    }
+
+    
+//    private fun showDatePickerDialog(v: View) {
+//        val fragment = DatePick()
+//        fragment.show(fragmentManager, "datePicker")
+//    }
 
 
 //    override fun onDateSet(view: DatePicker?, year: Int, month: Int, day: Int) {
@@ -66,8 +89,11 @@ class MainActivity : AppCompatActivity(), NewTodoDialogDelegate {
 //        pickDateButton.text = string
 //    }
 
+
+    // ---
+    //  NewTodoDialogDelegate
     override fun didPressAddTodo(todo: String, place: String, date: Date) {
-        val item = TodoListItem(this.itemAdapter!!.items.size+1, todo, place, date)
+        val item = TodoListItem(this.itemAdapter!!.items.size, todo, place, date)
         this.itemAdapter!!.items.add(item)
         this.itemAdapter!!.notifyDataSetChanged()
     }
